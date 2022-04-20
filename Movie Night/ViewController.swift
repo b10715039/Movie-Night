3e0ac0ba67148c1929115dc3c0d729be1a6d5823
b10715039 <b10715039@ntust.edu.tw>
2022-04-20 10:33:03 +0800

    Initial Commit

---
+++ Movie Night/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Night
+//
+//  Created by 凃佑瑋 on 2022/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
